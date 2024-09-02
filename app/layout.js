bb62592f750c1e9757609fb0f4ec714b787c61cb
Muhammad Talha Rehman <miantalhapaf@gmail.com>
2024-09-02 18:36:20 +0500

    Favicon changed svg

--- app/layout.js
+++ app/layout.js
@@ -25,11 +25,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
